54bcdd2268fd85f96a6fa0e830e1754b8ea9d334
forkbattery <forkbattery@gmail.com>
2014-12-11 09:24:38 -0500

    hello world

---
+++ FirstApp/FirstApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Kanishka Goel on 2014-12-10.
+//  Copyright (c) 2014 Kanishka Goel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
